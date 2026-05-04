ba1ba92261a2f2c9266453d3caa016af68b7f678
Ygor <ygorbravimr@gmail.com>
2026-05-04 10:18:05 +0200

    feat(tax): barrel export for src/lib/tax/

---
+++ src/lib/tax/index.ts
@@ -0,0 +1,11 @@
+export { computeDayFees } from "./fee-allocator"
+export type { FeeRates, DayFeeInput, DayFeeOutput } from "./fee-allocator"
+
+export { accumulateIrrf } from "./irrf-accumulator"
+export type { DailyResult, IrrfByDay, IrrfResult } from "./irrf-accumulator"
+
+export { computeDarf } from "./darf-calculator"
+export type { DarfInput, DarfOutput } from "./darf-calculator"
+
+export { buildCarryoverChain } from "./carryover-ledger"
+export type { MonthSummary, CarryoverState } from "./carryover-ledger"
